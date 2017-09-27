138caed6ebed437d29226247d0956c672fb44f11
Peter McCready <petermccready2@gmail.com>
2017-09-27 21:58:10 +0100

    Amends evaluate_position comment

--- board.rb
+++ board.rb
@@ -13,13 +13,13 @@ class Board
     return @board.length()
   end
 
-  # evaluating if landed on snake/ladder/exceeds board
+  # evaluating if landed on snake/ladder/passes finish
   def evaluate_position(player)
     if @board[player.position()] != nil  # nil go to end
       snake_or_ladder = @board[player.position()] # snake or ladder
       snake_or_ladder.move_player(player)
     end
-  end 
+  end
 
 
 end
